72ac568d613d11893a3ebc63dcd0b2b10bb710a4
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-05 20:14:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
 class Dog 
   
   def name=(dog_name)
-    @name = dog_name
+    @dogs_name = dog_name
   end 
   
   def name 
-    @name
+    @dogs_name
   end 
   
   def breed = (dog_breed)
-    @breed = dog_breed
+    @dogs_breed = dog_breed
   end
   
   def breed 
-    @breed
+    @dogs_breed
   end
   
   
